7b933fca1e8fefb03b27577d5630bbd880b88d6a
Ariqmau <ariqmalik@gmail.com>
2025-02-27 02:06:57 +0700

    fix file name

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,20 +1,23 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
+
     @Autowired
     private CarRepository carRepository;
 
     @Override
-    public Car create (Car car) {
-        // TODO Auto-generated method stub
+    public Car create(Car car) {
+        // TODO: Auto-generated method stub
         carRepository.create(car);
         return car;
     }
@@ -28,20 +31,20 @@ public class CarServiceImpl implements CarService {
     }
 
     @Override
-    public Car findById(String carId){
+    public Car findById(String carId) {
         Car car = carRepository.findById(carId);
         return car;
     }
 
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
+        // TODO: Auto-generated method stub
         carRepository.update(carId, car);
     }
 
     @Override
     public void deleteCarById(String carId) {
-        // TODO Auto-generated method stub
+        // TODO: Auto-generated method stub
         carRepository.delete(carId);
     }
 }
